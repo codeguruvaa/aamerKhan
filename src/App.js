f700de402ed429d796da899143f43cf84bf8d0ab
codeguruvaa <mohanvej@gmail.com>
2022-05-26 19:03:46 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Boxes from "./components/Boxes.js";
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import Slider from "./components/Slider.js";
+import TwoParas from "./components/TwoParas.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './components/App.css';
+
+function App(){
+  return(
+    <div>
+
+      <main>
+        <Header />
+        <Slider />
+        <TwoParas />
+        <div style={{clear:'both'}} ></div>
+        <Boxes />
+        <div style={{clear:'both'}} ></div>
+        <Footer />
+      </main>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/TwoParas.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+function TwoParas() {
+    return (
+        <div>
+
+        <div className='POne'>
+            <p>
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the 
+            </p>
+
+        </div>
+        <div className='PTwo'>
+            <p>
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+                the quick brown fox jumps over the 
+            </p>
+        </div>
+
+
+        </div>
+    );
+}
+
+export default TwoParas;
